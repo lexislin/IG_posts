bd485594cc2166a801d8634d4005db1b506362cd
lexislin <lexis_lin@foxmail.com>
2024-09-21 16:15:10 +0800

    Updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from collections import Counter
 import pandas as pd
 import streamlit as st
 
-# Make sure to download necessary nltk data first
+# Download necessary NLTK data (only needs to run once)
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 
